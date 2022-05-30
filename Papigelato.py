51afcd3fb11d2627d8a7e5bab5807ea71895dd1c
GarvenV <vitokroon@gmail.com>
2022-05-30 11:49:07 +0200

    'yes'

--- Papigelato.py
+++ Papigelato.py
@@ -1,5 +1,9 @@
 print ('Welkom bij Papi Gelato')
 print  ("je mag alle smaken kiezen zolang het maar vanille ijs is")
+   
+
+
+#------soortbolletjes--------
 def soortbolletjes (aantal):
     v = 1
     for x in range (1,aantal=1):
@@ -10,6 +14,7 @@ def soortbolletjes (aantal):
             print('Sorry dat is geen optie die we aanbieden..')
             soortbolletjes()
 
+#-----aantalbolletjes------
 def aantalbolletjes():
     aantal = int(input('Hoeveel bolletjes wilt u?'))
     if aantal >= 1 and aantal <= 3:
@@ -24,6 +29,8 @@ def aantalbolletjes():
     else:
         print('Sorry dat is geen optie die we aanbieden..')
 
+
+#------bakjehoorntjes------
 def bakjehoorntjes(aantal):
     bakjehoorntje = input('Wilt u deze ' + str(aantal) + ' bolletje(s) in A) een hoorntje of B) een bakje?')
     if bakjehoorntje == "A":
@@ -37,6 +44,7 @@ def bakjehoorntjes(aantal):
             
             print('Bedankt en tot ziens')
 
+
     elif bakjehoorntje == "B":
         totaal = prijs(aantal, 0, 1, 1)
         bakje = input('Hier is uw bakje met ' + str(aantal) + ' bolletje(s). Wilt u nog meer bestellen? (Y/N)')
@@ -48,29 +56,7 @@ def bakjehoorntjes(aantal):
     else:
         print('Sorry dat is geen optie die we aanbieden..')
 
-def prijs (aantal,hoortje,bol,top):
-    top = toppings(top) 
-    totaalprijs = 0 
-    bolletje = round(1.10, 2)
-    hoorn = 1.10
-    bakje = 0.55
-    if hoorntje == 1:
-        totaalprijs += hoorn
-    elif bol == 1:
-        totaalprijs =+ bakje
-        bolletjeprijs = bolletje * aantal
-        totaalprijs = totaalprijs + bolletjeprijs
-        totaalprijs = totaalprijs + top
-
-    print ("--------[Papi Gelato]--------")
-    print("Bolletjes        " + str(aantal) + " x €0.70 = €" + str(aantal * 0.70))
-    print("Hoorntje         " + str(hoorntje) + " x €1.10 = €" + str(hoorntje * 1.10))
-    print("Bakje            " + str(bol) + " x €0.70 = €" + str(bol * 0.55))
-    print("Toppings                   = €" + str(round(top,2)) )
-    print("Totaal                     = €" + str(round(totaalprijs,2)))
-
-
-    return totaalprijs 
+#-------toppings------    
 def toppings(bakje):
     top = input('wat voor topping wilt u: A) Geen B) Slagroom, C) Sprinkels of D) Caramel Saus?')
     if top == "A":
@@ -97,4 +83,29 @@ def toppings(bakje):
     else:
         print("Sorry dat begrijp ik niet.")
 
+
+#-------prijs-------
+def prijs (aantal,hoorntje,bol,top):
+    top = toppings(top) 
+    totaalprijs = 0 
+    bolletje = round(1.10, 2)
+    hoorn = 1.25
+    bakje = 0.75
+    if hoorntje == 1:
+        totaalprijs += hoorntje
+    elif bol == 1:
+        totaalprijs =+ bakje
+        bolletjeprijs = bolletje * aantal
+        totaalprijs = totaalprijs + bolletjeprijs
+        totaalprijs = totaalprijs + top
+
+    print ("--------[Papi Gelato]--------")
+    print("Bolletjes        " + str(aantal) + " x €0.70 = €" + str(aantal * 1.10))
+    print("Hoorntje         " + str(hoorntje) + " x €1.10 = €" + str(hoorntje * 1.25))
+    print("Bakje            " + str(bol) + " x €0.75 = €" + str(bol * 0.55))
+    print("Toppings                   = €" + str(round(top,2)) )
+    print("Totaal                     = €" + str(round(totaalprijs,2)))
+
+
+    return totaalprijs 
 
